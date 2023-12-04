ed67893bd72b2af250a30c0b67d14386910414fd
James <jjagielski19@gmail.com>
2023-12-04 11:47:56 -0500

    clearer code for the reading file

--- Parasitic_Capacitance/reading_ext.py
+++ Parasitic_Capacitance/reading_ext.py
@@ -1,34 +1,56 @@
-parasitic_nets = dict()
+'''parasitic_nets = dict()
 file1 = open("inverter.ext", "r+")
 lines = file1.readlines()
-
-
-def SPICE_read():
-    file1 = open("nmos.sch", "r+")
-    lines = file1.readlines()
-    for row in lines:
-        # check if string present on a current line
-        #word1 = 'name'
-        word = "cap"
-
-        # find() method returns -1 if the value is not found,
-        # if found it returns index of the first occurrence of the substring
-
-        # if row.find(word1) != -1:
-        #     print(row[row.index(word1) + len(word1):])
-        if row.find("cap") != -1:
-            key = row[row.index(word)+ len(word):len(row)-8:]
-            cap = row[len(row)-8:]
-            addnet(key,cap)
-        file1.close()
-    return(parasitic_nets)
-
 def addnet(name, number):
     for i in parasitic_nets:
         if i == name:
             print('error')
             return
     parasitic_nets[name] = number
+    return parasitic_nets
+for row in lines:
+    # check if string present on a current line
+    #word1 = 'name'
+    word = "cap"
+
+    # find() method returns -1 if the value is not found,
+    # if found it returns index of the first occurrence of the substring
+
+    # if row.find(word1) != -1:
+    #     print(row[row.index(word1) + len(word1):])
+    if row.find("cap") != -1:
+        key = row[row.index(word)+ len(word):len(row)-8:]
+        cap = row[len(row)-8:]
+        addnet(key,cap)
+    file1.close()
+print(parasitic_nets)'''
+
+
+parasitic_nets = dict()
+file1 = open("inverter.ext", "r+")
+lines = file1.readlines()
+def addnet(name, number):
+    for i in parasitic_nets:
+        if i == name:
+            print('error')
+            return
+    parasitic_nets[name] = number
+def SPICE_read():
+    for row in lines:
+        # check if string present on a current line
+        #word1 = 'name'
+        word = "cap"
+        # find() method returns -1 if the value is not found,
+        # if found it returns index of the first occurrence of the substring
+
+        # if row.find(word1) != -1:
+        #     print(row[row.index(word1) + len(word1):])
+        if row.find(word) != -1:
+            key = row[row.index(word)+ len(word):len(row)-8:]
+            cap = row[len(row)-8:]
+            addnet(key,cap)
+        file1.close()
+    return(parasitic_nets)
 
 
 
